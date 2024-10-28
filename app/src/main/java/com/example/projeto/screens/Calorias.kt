8d2de617c87f58f9edc43eac0ff151373b452612
Alexandre Freitas <8220154@estg,ipp,pt>
2024-10-28 19:56:11 +0000

    v.1.0.6

--- app/src/main/java/com/example/projeto/screens/Calorias.kt
+++ app/src/main/java/com/example/projeto/screens/Calorias.kt
@@ -17,19 +17,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.projeto.reuse.BottomNavigationBar
 import com.example.projeto.reuse.Header
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 @Composable
 fun CalorieScreen() {
